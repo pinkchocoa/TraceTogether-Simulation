fa1a229efb6644cc6a865b5187a3de3857afdea7
Sneakykk <74500256+Sneakykk@users.noreply.github.com>
2021-04-04 13:48:05 +0800

    tidy up codes (not 100% yet)

--- comparison.py
+++ comparison.py
@@ -2,45 +2,41 @@ import randomFn
 
 # in this file stores the functions we need to compare trace together data
 
+
 # function to check if timestamp coincides
 def checkTimeStamp():
     setoftimestamps1 = randomFn.random_timestamp(1, 1)[0]
     setoftimestamps2 = randomFn.random_timestamp(1, 1)[0]
 
-    # print(setoftimestamps1[0], setoftimestamps1[1])
-    # print(setoftimestamps2[0], setoftimestamps2[1])
+    print(setoftimestamps1[0], setoftimestamps1[1])
+    print(setoftimestamps2[0], setoftimestamps2[1])
 
-    #version 1
-    # results = []
-    # for timestamp in setoftimestamps1:
-    #     results.append(setoftimestamps2[0] < timestamp < setoftimestamps2[1])
-    # for timestamp in setoftimestamps2:
-    #     results.append(setoftimestamps1[0] < timestamp < setoftimestamps1[1])
-    #     return True in results
+    # version 1
+    results = []
+    for timestamp in setoftimestamps1:
+        results.append(setoftimestamps2[0] < timestamp < setoftimestamps2[1])
+    for timestamp in setoftimestamps2:
+        results.append(setoftimestamps1[0] < timestamp < setoftimestamps1[1])
+        return True in results
 
+    # version 2
+    # person1 = [
+    #     {'check_in_time': setoftimestamps1[0], 'check_out_time': setoftimestamps1[1]},
+    # ]
+    # person2 = [
+    #     {'check_in_time': setoftimestamps2[0], 'check_out_time': setoftimestamps2[1]},
+    # ]
+    #
+    # for i1, d1 in enumerate(person1):
+    #     for i2, d2 in enumerate(person2):
+    #         start1 = d1["check_in_time"]
+    #         start2 = d2["check_in_time"]
+    #         end1 = d1["check_out_time"]
+    #         end2 = d2["check_out_time"]
+    #
+    #         if start2 >= start1 and end2 >= end1:
+    #             print("person1", i1, "overlaps with person2", i2)
 
-    #version 2
-    Intervals1 = [
-        {'starting_time': '2021-04-04 07:10:39.286168', 'ending_time': '2021-04-04 07:10:40.276504'},
-        {'starting_time': '2021-04-04 07:10:40.301116', 'ending_time': '2016-02-26 07:10:40.722193'},
-    ]
-    Intervals2 = [
-        {'starting_time': '2021-04-04 07:10:39.590220', 'ending_time': '2021-04-04  07:10:41.482954'},
-        {'starting_time': '2021-04-04 07:10:41.649375', 'ending_time': '2021-04-04 07:10:42.615738'},
-    ]
-
-    for i1, d1 in enumerate(Intervals1):
-        for i2, d2 in enumerate(Intervals2):
-            start1 = d1["starting_time"]
-            start2 = d2["starting_time"]
-            end1 = d1["ending_time"]
-            end2 = d2["ending_time"]
-
-            if end1 >= start2 and end2 >= start1:
-                print("Intervals1", i1, "overlaps with Intervals2", i2)
-
-    # print(setoftimestamps1[0], setoftimestamps1[1])
-    # print(setoftimestamps2[0], setoftimestamps2[1])
     # # ^i used the above to generate 2 random sets of timestamps,
     # # u can create ur own too for checking
     # # they should be datetime objects
