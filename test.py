5dcdf1b54c40573ab6c7bf7dab1462e3fcf20081
jodie <46685749+pinkchocoa@users.noreply.github.com>
2021-04-07 01:28:39 +0800

    moving rand but need to fix rand

--- test.py
+++ test.py
@@ -3,6 +3,7 @@ from typing import Tuple
 import pygame
 import sys
 import os
+import randomFn
 
 '''
 Variables
@@ -44,6 +45,7 @@ class Player(pygame.sprite.Sprite):
         """
         control player movement
         """
+        #bounding box
         self.movex += x
         self.movey += y
 
@@ -51,7 +53,18 @@ class Player(pygame.sprite.Sprite):
         """
         Update sprite position
         """
-
+        if (self.rect.x + self.movex) < 0:
+            self.rect.x = 0
+            return
+        elif  (self.rect.x + self.movex) > worldx:
+            self.rect.x = worldx-20
+            return
+        if (self.rect.y + self.movey) < 0:
+            self.rect.y = 0
+            return
+        elif  (self.rect.y + self.movey) > worldy:
+            self.rect.y = worldy-20
+            return
         self.rect.x = self.rect.x + self.movex
         self.rect.y = self.rect.y + self.movey
 
@@ -71,12 +84,16 @@ player.rect.x = 0  # go to x
 player.rect.y = 0  # go to y
 player_list = pygame.sprite.Group()
 player_list.add(player)
-steps = 10
+steps = 1
+
+def moveInDirection(d):
+    direction={1:[steps,0], 2:[-steps,0], 3:[0,steps], 4:[0,-steps]}
+    player.control(direction[d][0], direction[d][1])
+
 
 '''
 Main Loop
 '''
-
 while main:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -85,35 +102,14 @@ while main:
                 sys.exit()
             finally:
                 main = False
+    prevx,prevy = player.rect.x, player.rect.y
+    while(prevx == player.rect.x and prevy == player.rect.y):
+        moveInDirection(randomFn.randInt(1,4))
+        player.update()
 
-        if event.type == pygame.KEYDOWN:
-            if event.key == ord('q'):
-                pygame.quit()
-                try:
-                    sys.exit()
-                finally:
-                    main = False
-            if event.key == pygame.K_LEFT or event.key == ord('a'):
-                player.control(-steps, 0)
-            if event.key == pygame.K_RIGHT or event.key == ord('d'):
-                player.control(steps, 0)
-            if event.key == pygame.K_UP or event.key == ord('s'):
-                player.control(0, steps)
-            if event.key == pygame.K_UP or event.key == ord('w'):
-                player.control(0, -steps)
-
-        if event.type == pygame.KEYUP:
-            if event.key == pygame.K_LEFT or event.key == ord('a'):
-                player.control(steps, 0)
-            if event.key == pygame.K_RIGHT or event.key == ord('d'):
-                player.control(-steps, 0)
-            if event.key == pygame.K_UP or event.key == ord('s'):
-                player.control(0, -steps)
-            if event.key == pygame.K_UP or event.key == ord('w'):
-                player.control(0, steps)
 
     world.blit(backdrop, backdropbox)
-    player.update()
+    
     player_list.draw(world)
     pygame.display.flip()
     clock.tick(fps)
