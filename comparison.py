a0c2f0352d452b0439866b74272445985575f7a2
jodie <46685749+pinkchocoa@users.noreply.github.com>
2021-04-08 05:56:25 +0800

    Update comparison.py

--- comparison.py
+++ comparison.py
@@ -1,16 +1,11 @@
 import datetime
 
-# in this file stores the functions we need to compare trace together data
-
-
 # function to check if timestamp coincides
-
-def checkTimeStamp(setoftimestamps1,setoftimestamps2): # function to check if timestamp coincides
-    # version 1
+def checkTimeStamp(setoftimestamps1,setoftimestamps2):
     results = []
-    for timestamp in setoftimestamps1: #iterate through the first set of timestamps 
-        results.append(setoftimestamps2[0] < timestamp < setoftimestamps2[1]) #appending the value into results[]
-    for timestamp in setoftimestamps2: #iterate through the second set of timestamps 
-        results.append(setoftimestamps1[0] < timestamp < setoftimestamps1[1]) #appending the value into results[]
+    for timestamp in setoftimestamps1: 
+        results.append(setoftimestamps2[0] < timestamp < setoftimestamps2[1]) 
+    for timestamp in setoftimestamps2: 
+        results.append(setoftimestamps1[0] < timestamp < setoftimestamps1[1]) 
         return True in results
         
