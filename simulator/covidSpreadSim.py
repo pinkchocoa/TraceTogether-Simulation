ec0dd8ca25c8de7fa3808421578895aa529a912d
jodie <46685749+pinkchocoa@users.noreply.github.com>
2021-04-07 23:19:39 +0800

    json files

--- simulator/covidSpreadSim.py
+++ simulator/covidSpreadSim.py
@@ -6,6 +6,7 @@ import sys
 import os
 import randomFn
 import numpy
+import object
 
 # global variables
 worldx = 720
@@ -19,49 +20,7 @@ images.append(pygame.image.load(os.path.join('images', 'red20.png')).convert_alp
 images.append(pygame.image.load(os.path.join('images', 'yellow20.png')).convert_alpha())
 
 
-# object class
-class Player(pygame.sprite.Sprite):
-    """
-    Spawn a person
-    """
 
-    def __init__(self):
-        pygame.sprite.Sprite.__init__(self)
-        self.movex = 0
-        self.movey = 0
-        self.frame = 0
-        self.image = images[0]
-        self.rect = self.image.get_rect()
-
-    def control(self, x, y):
-        """
-        control player movement
-        """
-        #bounding box
-        self.movex = x
-        self.movey = y
-
-    def update(self):
-        """
-        Update sprite position
-        """
-        if (self.rect.x + self.movex) < 0:
-            moveInDirection(randomFn.randInt(1,8))
-            return
-        elif  (self.rect.x + self.movex) > worldx-20:
-            moveInDirection(randomFn.randInt(1,8))
-            return
-        if (self.rect.y + self.movey) < 0  :
-            moveInDirection(randomFn.randInt(1,8))
-            return
-        elif  (self.rect.y + self.movey) > worldy-20:
-            moveInDirection(randomFn.randInt(1,8))
-            return
-        if (randomFn.randChance(5)):
-            moveInDirection(randomFn.randInt(1,8))
-            return
-        self.rect.x = self.rect.x + self.movex
-        self.rect.y = self.rect.y + self.movey
 
 '''
 Setup
